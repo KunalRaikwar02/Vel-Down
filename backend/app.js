0fe56a04e33d2b0dfc12815ff842fa3e47f32a2b
Kunal Raikwar <kunalraikwar12367@gmail.com>
2026-03-21 01:33:37 +0530

    Fix: added path module and dynamic port for deployment

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const dotenv = require('dotenv');
 const youtubeRoutes = require('./routes/youtubeRoutes');
 
