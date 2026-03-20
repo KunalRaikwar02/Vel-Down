ee171f136cd64b60e0519538bbec9572e743c009
Kunal Raikwar <kunalraikwar12367@gmail.com>
2026-03-21 00:58:52 +0530

    finally complete

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,7 @@ const app = express();
 // Middleware configuration
 app.use(cors()); 
 app.use(express.json());
+app.use(express.static('./public'));
 
 // Main API Routes
 app.use('/api/youtube', youtubeRoutes);
